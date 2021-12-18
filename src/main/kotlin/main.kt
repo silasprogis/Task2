a33e4bc910950ee126b1ea391879b224f36c9af4
Alex <silasprogis@gmail.com>
2021-12-18 22:58:45 +0300

    change amount

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,13 @@
 package ru.netology
 
 fun main () {
-    val amount = 10000
+    val amount = 100000
     val interest = 0.0075f
     val minInterest = 3500
     //val message = "Transaction is: "
     //val messageInterest = ", with interest: "
 
-    val result = if ((amount * interest) > minInterest) {
+    if ((amount * interest) > minInterest) {
         print("Transaction is: " + (amount - amount * interest) + ", with interest: " + (amount * interest) )
     } else print("Transaction is: " + (amount - minInterest) + ", with interest: " + minInterest)
 }
