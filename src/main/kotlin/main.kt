6a00638a6a9b2b4b107af7168da7c32e0f92d93c
Alex <silasprogis@gmail.com>
2021-12-19 15:20:41 +0300

    final

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,21 @@
 package ru.netology
 
 fun main () {
-    val amount = 100000
+    val amount = 10000
     val interest = 0.0075f
-    val minInterest = 3500
-    //val message = "Transaction is: "
-    //val messageInterest = ", with interest: "
-
+    val minInterest = 3500f
+    val resultInterest:Float
     if ((amount * interest) > minInterest) {
-        print("Transaction is: " + (amount - amount * interest) + ", with interest: " + (amount * interest) )
-    } else print("Transaction is: " + (amount - minInterest) + ", with interest: " + minInterest)
+        resultInterest = amount * interest
+    } else { resultInterest = minInterest }
+
+    val amountAfterNet = (amount - (resultInterest))
+    val amountAfterRub = amountAfterNet / 100
+    val amountAfterKop = (amountAfterRub % 1) * 100
+    val resultInterestRub = resultInterest / 100
+    val resultInterestKop = (resultInterestRub % 1) * 100
+
+    print("Transaction is: " + String.format("%.0f", amountAfterRub) + " Rub "
+            + String.format("%.0f", amountAfterKop) + " Kop., " + "with an interest: "
+            + String.format("%.0f", resultInterestRub) + " Rub " + String.format("%.0f", resultInterestKop) + " Kop." )
 }
